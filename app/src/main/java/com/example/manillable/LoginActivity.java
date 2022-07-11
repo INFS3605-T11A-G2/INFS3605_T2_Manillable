44e527efc2802da5ee726dd357bfde80e3ed2977
PatrickDiakos <90656772+PatrickDiakos@users.noreply.github.com>
2022-07-11 18:05:05 +1000

    LoginScreen - logo

--- app/src/main/java/com/example/manillable/LoginActivity.java
+++ app/src/main/java/com/example/manillable/LoginActivity.java
@@ -3,8 +3,21 @@ package com.example.manillable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private TextView register;
+    private Button loginButton;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private static final String USER = "user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
